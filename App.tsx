81d4f3aeec0c38e35d3208af771e9ea05a71cb37
Michael Boguslaw <mboguslaw97@gmail.com>
2020-11-26 23:19:36 -0500

    amplify init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import Amplify from "aws-amplify";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import config from "./aws-exports";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+Amplify.configure(config);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
