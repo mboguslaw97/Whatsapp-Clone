e944f167043900c2e0155b7239ca1b32cf2f48f8
Michael Boguslaw <mboguslaw97@gmail.com>
2020-11-28 21:20:14 -0500

    fetch and display messages

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,66 +1,86 @@
+import { API, Auth, graphqlOperation } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
 import {
-  Entypo,
-  FontAwesome5,
-  Fontisto,
-  MaterialCommunityIcons,
-  MaterialIcons,
-} from "@expo/vector-icons";
-import React, { useState } from "react";
-import { View } from "react-native";
+    TextInput, TouchableOpacity, TouchableWithoutFeedback
+} from 'react-native-gesture-handler';
+
 import {
-  TextInput,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-} from "react-native-gesture-handler";
-import styles from "./styles";
+    Entypo, FontAwesome5, Fontisto, MaterialCommunityIcons, MaterialIcons
+} from '@expo/vector-icons';
 
-const InputBox = () => {
-  const [message, setMessage] = useState("");
+import { createMessage } from '../../graphql/mutations';
+import styles from './styles';
 
-  const onMicrophonePress = () => {
-    console.warn("Microphone");
-  };
+const InputBox = (props) => {
+	const { chatRoomID } = props;
+	const [message, setMessage] = useState("");
+	const [myUserID, setMyUserID] = useState(null);
 
-  const onSendPress = () => {
-    console.warn(`Sending ${message}`);
-    setMessage("");
-  };
+	useEffect(() => {
+		const fetchUser = async () => {
+			const userInfo = await Auth.currentAuthenticatedUser();
+			setMyUserID(userInfo.attributes.sub);
+		};
+		fetchUser();
+	}, []);
 
-  const onPress = () => {
-    if (!message) {
-      onMicrophonePress();
-    } else {
-      onSendPress();
-    }
-  };
+	const onMicrophonePress = () => {
+		console.warn("Microphone");
+	};
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.mainContainer}>
-        <FontAwesome5 name="laugh-beam" size={24} color="grey" />
-        <TextInput
-          placeholder="Type a message"
-          style={styles.textInput}
-          value={message}
-          onChangeText={setMessage}
-          multiline
-        />
-        <Entypo name="attachment" size={24} color="grey" style={styles.icon} />
-        {!message && (
-          <Fontisto name="camera" size={24} color="grey" style={styles.icon} />
-        )}
-      </View>
-      <TouchableOpacity onPress={onPress}>
-        <View style={styles.buttonContainer}>
-          {message ? (
-            <MaterialCommunityIcons name="microphone" size={24} color="white" />
-          ) : (
-            <MaterialIcons name="send" size={24} color="white" />
-          )}
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
+	const onSendPress = async () => {
+		try {
+			await API.graphql(
+				graphqlOperation(createMessage, {
+					input: {
+						content: message,
+						userID: myUserID,
+						chatRoomID: chatRoomID,
+					},
+				})
+			);
+		} catch (error) {
+			console.log(error);
+		}
+		setMessage("");
+	};
+
+	const onPress = () => {
+		if (!message) {
+			onMicrophonePress();
+		} else {
+			onSendPress();
+		}
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.mainContainer}>
+				<FontAwesome5 name="laugh-beam" size={24} color="grey" />
+				<TextInput
+					placeholder="Type a message"
+					style={styles.textInput}
+					value={message}
+					onChangeText={setMessage}
+					multiline
+				/>
+				<Entypo name="attachment" size={24} color="grey" style={styles.icon} />
+				{!message && (
+					<Fontisto name="camera" size={24} color="grey" style={styles.icon} />
+				)}
+			</View>
+			<TouchableOpacity onPress={onPress}>
+				<View style={styles.buttonContainer}>
+					{!message ? (
+						<MaterialCommunityIcons name="microphone" size={24} color="white" />
+					) : (
+						<MaterialIcons name="send" size={24} color="white" />
+					)}
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 export default InputBox;
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,22 +1,52 @@
-import { useRoute } from "@react-navigation/native";
-import React from "react";
-import { FlatList, ImageBackground, Text } from "react-native";
-import ChatMessage from "../components/ChatMessage";
-import chatRoomData from "../data/Chats";
-import BG from "../assets/images/BG.png";
-import InputBox from "../components/InputBox";
+import { API, Auth, graphqlOperation } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import { FlatList, ImageBackground, KeyboardAvoidingView, Text } from 'react-native';
+
+import { useRoute } from '@react-navigation/native';
+
+import BG from '../assets/images/BG.png';
+import ChatMessage from '../components/ChatMessage';
+import InputBox from '../components/InputBox';
+import { messagesByChatRoom } from '../graphql/queries';
 
 const ChatRoomScreen = () => {
-  const route = useRoute();
-  return (
-    <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
-      <FlatList
-        data={chatRoomData.messages}
-        renderItem={({ item }) => <ChatMessage message={item} />}
-      />
-      <InputBox />
-    </ImageBackground>
-  );
+	const [myUserID, setMyUserID] = useState(null);
+	const [messages, setMessages] = useState([]);
+
+	const route = useRoute();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const userInfo = await Auth.currentAuthenticatedUser();
+			setMyUserID(userInfo.attributes.sub);
+		};
+		fetchUser();
+	}, []);
+
+	useEffect(() => {
+		const fetchMessages = async () => {
+			const messagesData = await API.graphql(
+				graphqlOperation(messagesByChatRoom, {
+					chatRoomID: route.params.id,
+					sortDirection: "ASC",
+				})
+			);
+			setMessages(messagesData.data.messagesByChatRoom.items);
+		};
+		fetchMessages();
+	}, []);
+
+	return (
+		<ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
+			<FlatList
+				data={messages}
+				renderItem={({ item }) => <ChatMessage myUserID={myUserID} message={item} />}
+			/>
+			<KeyboardAvoidingView behavior="position">
+				<InputBox chatRoomID={route.params.id} />
+			</KeyboardAvoidingView>
+		</ImageBackground>
+	);
 };
 
 export default ChatRoomScreen;
